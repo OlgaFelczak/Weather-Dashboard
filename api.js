63d7b6cf7afa7cddc1d3f129e0984514f1e1d59f
Olga Felczak <ofelczak@gmail.com>
2023-01-31 23:33:06 +0000

    change api to go over https

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const OpenWeatherApi = {
   getCoordinates: async (cityName) => {
     const data = await $.ajax({
       method: "GET",
-      url: `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${OPENWEATHER_API_KEY}`,
+      url: `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${OPENWEATHER_API_KEY}`,
     });
 
     return data[0];
@@ -13,7 +13,7 @@ const OpenWeatherApi = {
   getForecast: async (lat, lon) => {
     const list = await $.ajax({
       method: "GET",
-      url: `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}`,
+      url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}`,
     });
 
     return list;
