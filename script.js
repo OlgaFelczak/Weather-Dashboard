38763d55be4fc03af7bf6cbec10002ae6921ea69
Olga Felczak <ofelczak@gmail.com>
2023-01-31 23:09:18 +0000

    Buttons are rendered after page reload

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
-// Create button to search city
+$(document).ready(() => {
+  for (var i = 0, len = localStorage.length; i < len; ++i) {
+    const city = localStorage.key(i);
+    const forecast = JSON.parse(localStorage.getItem(city));
+
+    renderButton(city, forecast);
+  }
+});
+
 $("#search-button").click(async (e) => {
   e.preventDefault();
   const searchInputValue = $("#search-input").val();
+  const cityName = capitalizeFirstLetter(searchInputValue);
 
-  const { lon, lat } = await OpenWeatherApi.getCoordinates(searchInputValue);
+  const response = await OpenWeatherApi.getCoordinates(cityName);
+  if (!response) {
+    alert("I cannot recognize this city, sorry...");
+    return;
+  }
+  const { lat, lon } = response;
 
   const { list: weatherList } = await OpenWeatherApi.getForecast(lat, lon);
 
-  const transformedWeathers = [];
-  const today = new Date().getDate();
-  for (let i = 0; i < weatherList.length; i++) {
-    const weatherItem = weatherList[i];
-    if (i === 0) {
-      const newTempWeather = {
-        date: moment().format("D/M/YYYY"),
-        iconUrl: `https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png`,
-        temp: (weatherItem.main.temp - 273.15).toFixed(2),
-        wind: weatherItem.wind.speed.toFixed(2),
-        humidity: weatherItem.main.humidity,
-      };
-      transformedWeathers.push(newTempWeather);
-    }
-
-    if (
-      moment(weatherItem.dt_txt).format("H") === "15" &&
-      moment(weatherItem.dt_txt).format("DD") !== today
-    ) {
-      const newTempWeather = {
-        date: moment(weatherItem.dt_txt).format("D/M/YYYY"),
-        iconUrl: `https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png`,
-        temp: (weatherItem.main.temp - 273.15).toFixed(2),
-        wind: weatherItem.wind.speed.toFixed(2),
-        humidity: weatherItem.main.humidity,
-      };
-      transformedWeathers.push(newTempWeather);
-    }
-
-    if (transformedWeathers.length === 5) break;
-  }
-
-  saveToLocalStorageAndRenderButton(searchInputValue, transformedWeathers);
-  renderForecast(searchInputValue, transformedWeathers);
+  const transformedWeathers = transforOpenWeatherResponse(weatherList);
+  saveToLocalStorageAndRenderButton(cityName, transformedWeathers);
+  renderForecast(cityName, transformedWeathers);
 });
 
 function saveToLocalStorageAndRenderButton(city, forecast) {
-  localStorage.setItem(city, forecast);
+  localStorage.setItem(city, JSON.stringify(forecast));
 
+  renderButton(city, forecast);
+}
+
+function renderButton(city, forecast) {
   const button = $("<button>")
     .addClass("btn btn-secondary")
     .text(city)
@@ -95,4 +82,44 @@ function createSingleDay(singleDayForecast) {
   const card = $("<div>").addClass("card weather-card").append(cardBody);
 
   return card;
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function transforOpenWeatherResponse(weatherList) {
+  const transformedWeathers = [];
+  const today = new Date().getDate();
+
+  for (let i = 0; i < weatherList.length; i++) {
+    const weatherItem = weatherList[i];
+    if (i === 0) {
+      const newTempWeather = {
+        date: moment().format("D/M/YYYY"),
+        iconUrl: `https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png`,
+        temp: (weatherItem.main.temp - 273.15).toFixed(2),
+        wind: weatherItem.wind.speed.toFixed(2),
+        humidity: weatherItem.main.humidity,
+      };
+      transformedWeathers.push(newTempWeather);
+    }
+
+    if (
+      moment(weatherItem.dt_txt).format("H") === "15" &&
+      moment(weatherItem.dt_txt).format("DD") !== today
+    ) {
+      const newTempWeather = {
+        date: moment(weatherItem.dt_txt).format("D/M/YYYY"),
+        iconUrl: `https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png`,
+        temp: (weatherItem.main.temp - 273.15).toFixed(2),
+        wind: weatherItem.wind.speed.toFixed(2),
+        humidity: weatherItem.main.humidity,
+      };
+      transformedWeathers.push(newTempWeather);
+    }
+
+    if (transformedWeathers.length === 5) break;
+  }
+  return transformedWeathers;
 }
